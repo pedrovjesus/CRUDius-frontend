c6593554ae6a38c40cb7acab47a31af71e7bd101
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-07-20 12:22:00 -0300

    adjusting design

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,7 +1,18 @@
 export const Terminal = () => {
   return (
-    <div className="bg-black text-green-400 font-mono px-4 py-3 rounded-sm border border-b-black border-fuchsia-500 h-full overflow-y-auto">
-      <p>$ Terminal</p>
+    <div className="bg-black font-mono px-4 py-3 rounded-sm border border-b-black border-fuchsia-500 h-full overflow-y-auto">
+      <div className="flex">
+        <span className="text-green-400 select-none mr-1">crudius@root:~$</span>
+        <input
+          type="text"
+          name="command"
+          className="bg-black text-white flex-1 border-0 focus:outline-none focus:ring-0"
+          spellCheck="false"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+        />
+      </div>
     </div>
   );
 };
