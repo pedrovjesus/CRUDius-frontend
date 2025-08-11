0d1afc8b169f8f136cd62c6a9ccc72e1816e3151
viceduartt <viceduartt@gmail.com>
2025-08-11 05:30:52 -0300

    fix bugs

--- src/components/EntitySettings.tsx
+++ src/components/EntitySettings.tsx
@@ -1,48 +1,168 @@
 import { useEffect, useState } from "react";
+import IconAdd from '../images/add.svg'
 
 export const EntitySettings = (nodes) => {
   const tables = nodes.tables
   const setTables = nodes.setTables
   const [tableFocus, setTableFocus] = useState(null)
+  const [inputFocus, setInpuFocus] = useState(null)
+  let stop = false
 
   useEffect(() => {
     setTimeout(() => {
       tables.map((table) => {
-        if (table.focus === true && tableFocus === null) {
-          setTableFocus(table)
+        if (table.focus === true) {
+
+          if (tableFocus !== null) {
+            if (table.id === tableFocus.id) {
+              stop = true
+            } else {
+              stop = false
+            }
+          }
+
+          if (stop === false) {
+            setTableFocus(table)
+          }
         }
       })
-    }, 1000)
+    }, 10)
   })
 
-  const inputs = () => {
-    let tableExisted = tableFocus === null? null: true
+  const getInputsValues = () => {
+    let inputs = []
 
-    if (tableExisted === true) {
-      {tableFocus.values.map((data) => {
-      })}
-      return (
-        <h1>
-          skksk
-        </h1>
-      )
-    } else {
-      <></>
+    if (tableFocus !== null) {
+      tableFocus.values.map((data) => {
+        if (data !== ' ') {
+          if (inputFocus === data.name) {
+            inputs.push(
+              <input key={data.name} autoFocus id={data.name} onChange={(e) => {handleInputValue(data.name, e.target)}} value={data.data}/>
+            )
+          } else {
+            inputs.push(
+              <input key={data.name} id={data.name} onChange={(e) => {handleInputValue(data.name, e.target)}} value={data.data}/>
+            )
+          }
+
+        }
+      })
     }
+
+    return inputs
   }
 
   function handleInputName (e) {
-    
+    setTableFocus({
+      ...tableFocus,
+      name: e.value
+    })
+
+    console.log(tableFocus.name)
+
+
     setTables((prevTable) =>
       prevTable.map((table) =>
-        table.ud === tableFocus.id
+        table.focus === true
           ? {
               ...table,
-              name: e.value,
+              name: tableFocus.name,
             }
           : table
       )
     );
+  }
+
+  function getInputsType (type) {
+    if (tableFocus !== null) {
+      if (tableFocus.type === type) {
+        return ' focus'
+      } else {
+        return ''
+      }
+    }
+  }
+
+  function handleInputType (e) {
+    const newType = e.classList[1]
+    
+    setTableFocus({
+      ...tableFocus,
+      type: newType,
+    })
+
+    setTables((prevTable) =>
+      prevTable.map((table) =>
+        table.focus === true
+          ? {
+              ...table,
+              type: newType,
+            }
+          : table
+      )
+    );
+  }
+
+  function handleInputValue (name, e) {
+    let values = [...tableFocus.values]
+
+    if (tableFocus !== null) {
+      tableFocus.values.map((value) => {
+        if (value.name === name) {
+          value.data = e.value
+          value.name = e.value
+          setInpuFocus(e.value)
+        }
+      })
+
+      let newTable = {
+        ...tableFocus,
+        values: values,
+      }
+
+      setTableFocus(newTable)
+  
+      setTables((prevTable) =>
+        prevTable.map((table) =>
+          table.focus === true
+            ? {
+                ...table,
+                values: values
+              }
+            : table
+        )
+      );
+    }
+
+  }
+
+  function addValueTable () {
+    if (tableFocus !== null) {
+      let values = [
+        ...tableFocus.values, 
+        {name: 'default ' + tableFocus.values.length, type: 'string', data: 'default'},
+      ]
+
+      setInpuFocus(values[values.length -1].name)
+
+      let newTable = {
+        ...tableFocus,
+        values: values
+      }
+
+      setTableFocus(newTable)
+  
+      setTables((prevTable) =>
+        prevTable.map((table) =>
+          table.focus === true
+            ? {
+                ...table,
+                values: values
+              }
+            : table
+        )
+      );
+    }
   }
 
   return (
@@ -57,17 +177,17 @@ export const EntitySettings = (nodes) => {
           <div className="inputs">
             <div className="section-input">
               <input className="input-type-entity" type="radio" name="type-create" id="type-create" value='create' />
-              <label htmlFor="type-create">C</label>
+              <label htmlFor="type-create" onClick={(e) => {handleInputType(e.target)}} className={'label create' + getInputsType('create')}>C</label>
             </div>
 
             <div className="section-input">
               <input className="input-type-entity" type="radio" name="type-update" id="type-update" value='update' />
-              <label htmlFor="type-update">U</label>
+              <label htmlFor="type-update" onClick={(e) => {handleInputType(e.target)}} className={'label update' + getInputsType('update')}>U</label>
             </div>
 
             <div className="section-input">
               <input className="input-type" type="radio" name="type-delete" id="type-delete" value='delete' />
-              <label htmlFor="type-delete">D</label>
+              <label htmlFor="type-delete" onClick={(e) => {handleInputType(e.target)}} className={'label delete' + getInputsType('delete')}>D</label>
             </div>
           </div>
         </div>
@@ -83,12 +203,14 @@ export const EntitySettings = (nodes) => {
         </div>
 
         <div className="label-inputs entity-values">
-          <h3>Values</h3>
+          <div className="add-values">
+            <h3>Values</h3>
+            <button onClick={() => {addValueTable()}} className="button-value"><img src={IconAdd} alt="" /></button>
+          </div>
 
           <div className="inputs">
             <div className="section-input">
-              {inputs()}
-              <button className="button-value">New value</button>
+              {getInputsValues()}
             </div>
           </div>
         </div>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,17 +6,17 @@ import { Canva } from "./Canva";
 
 export const Layout = () => {
   const [nodes, setNodes] = useState([
-    { id: '1Nome',focus: false, x: 300, y: -100, name: "Pessoa", type: 'create', values: [
-      {name: 'pessoa', type: 'string', data: 'teste'},
+    { id: '1Nome',focus: true, x: 300, y: -100, name: "Pessoa", type: 'create', values: [
+      {name: 'pessoa0', type: 'string', data: 'teste'},
       {name: 'pessoa1', type: 'string', data: 'tststs'},
+      {name: 'pessoa1saasasasa', type: 'string', data: 'tststasasass'},
     ] },
     { id: '2Nome', focus: false, x: -200, y: -200, name: "Dog", type: 'update', values: [
       {name: 'pessoa2', type: 'string', data: 'teste'},
       {name: 'pessoa3', type: 'string', data: 'tststs'},
     ] },
-    { id: '3Nome', focus: true, x: 0, y: 0, name: "Worlf", type: 'delete', values: [
+    { id: '3Nome', focus: false, x: 0, y: 0, name: "Worlf", type: 'delete', values: [
       {name: 'pessoa2', type: 'string', data: 'teste'},
-      {name: 'pessoa3', type: 'string', data: 'tststs'},
     ] },
   ]);
 
